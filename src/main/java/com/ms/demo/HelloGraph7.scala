6573c973b01156ce73152c85cbf8c51b85d6378c
Jeremy davis <jerdavis>
2015-11-10 15:21:00 -0800

    Hello World on actual cluster

---
+++ src/main/java/com/ms/demo/HelloGraph7.scala
@@ -0,0 +1,30 @@
+package com.ms.demo
+
+import org.apache.spark._
+import org.apache.spark.graphx.{Edge, Graph, VertexId}
+
+/**
+ * http://spark.apache.org/docs/latest/quick-start.html
+ * /opt/spark/bin/spark-submit --verbose --driver-class-path $(find /opt/hadoop/share/hadoop/mapreduce/lib/hadoop-lzo-* | head -n 1) --class com.ms.demo.HelloGraph7 ~/tmp/graph2-1.0-SNAPSHOT.jar
+ */
+object HelloGraph7 {
+
+  def main( args: Array[String] ) = {
+    println("Hello World Graph")
+
+    //val conf = new SparkConf().setAppName("Simple Application")
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc   = new SparkContext(conf)
+
+    val lines = sc.textFile("hdfs:///user/jeremy/spark/")
+    val lineLengths = lines.map(s => s.length)
+    val totalLength = lineLengths.reduce((a, b) => a + b)
+
+    println("NumRows:" + lines.count() + "," + lineLengths + "," + totalLength )
+
+  }
+}
+
+
+
+
