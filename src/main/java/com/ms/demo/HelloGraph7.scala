eb066acdfe0e7b1f605e80bc918fa7b61e6c82a7
Jeremy davis <jerdavis>
2015-11-10 17:41:17 -0800

    Hello World on actual cluster

--- src/main/java/com/ms/demo/HelloGraph7.scala
+++ src/main/java/com/ms/demo/HelloGraph7.scala
@@ -5,7 +5,7 @@ import org.apache.spark.graphx.{Edge, Graph, VertexId}
 
 /**
  * http://spark.apache.org/docs/latest/quick-start.html
- * /opt/spark/bin/spark-submit --verbose --driver-class-path $(find /opt/hadoop/share/hadoop/mapreduce/lib/hadoop-lzo-* | head -n 1) --class com.ms.demo.HelloGraph7 ~/tmp/graph2-1.0-SNAPSHOT.jar
+ * /opt/spark/bin/spark-submit --verbose --driver-class-path $(find /opt/hadoop/share/hadoop/mapreduce/lib/hadoop-lzo-* | head -n 1) --class com.ms.demo.HelloGraph7 ~/tmp/graph2-1.0-SNAPSHOT.jar hdfs:///user/jeremy/spark/
  */
 object HelloGraph7 {
 
@@ -16,7 +16,7 @@ object HelloGraph7 {
     val conf = new SparkConf().setAppName("Simple Application")
     val sc   = new SparkContext(conf)
 
-    val lines = sc.textFile("hdfs:///user/jeremy/spark/")
+    val lines = sc.textFile(args(0))
     val lineLengths = lines.map(s => s.length)
     val totalLength = lineLengths.reduce((a, b) => a + b)
 
