97dfe422540ae9536500e6fecdbd99ed747ef800
Jeremy davis <jerdavis>
2016-01-12 15:31:31 -0800

    First working Unit Test

--- src/test/scala/com.ms.demo/TestGraph.scala
+++ src/test/scala/com.ms.demo/TestGraph.scala
@@ -2,8 +2,8 @@ package com.ms.demo
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
-import org.junit.{After, Before, Test, Assert}
-//import org.junit.Assert
+import org.junit.{After, Before, Test }
+import org.junit.Assert._
 
 
 /**
@@ -15,7 +15,6 @@ class TestGraph {
 
   @Before
   def before(): Unit = {
-    println("Before")
     System.clearProperty("spark.driver.port")
     System.clearProperty("spark.hostPort")
     sc = createSparkContext()
@@ -23,8 +22,6 @@ class TestGraph {
 
   @After
   def after(): Unit = {
-    println("After")
-
     sc.stop()
     sc = null
     System.clearProperty("spark.driver.port")
@@ -33,20 +30,16 @@ class TestGraph {
 
 
   @Test
-  def testNothing(): Unit = {
-
-    println("Hello Worldsss")
-
-
+  def testParsing(): Unit = {
 
     val lines = Array("row1,click,1234,account,1",
                       "row2,click,1235,account,2" )
     val inputData :RDD[String] = sc.parallelize(lines)
     val parsed : RDD[(String,String,String,String,String)] = GraphDemo.parseRawData(',',inputData)
 
-    Assert.assertEquals(2,parsed.count())
-    Assert.assertTrue(parsed.collect().contains(("row1","click","1234","account","1") ))
-    Assert.assertTrue(parsed.collect().contains(("row2","click","1235","account","2") ))
+    assertEquals(2,parsed.count())
+    assertTrue(parsed.collect().contains(("row1","click","1234","account","1") ))
+    assertTrue(parsed.collect().contains(("row2","click","1235","account","2") ))
   }
 
   /**
